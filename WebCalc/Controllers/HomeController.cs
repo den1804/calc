f7187f67614d885b4a2a521b8db8812deda2a532
den1804 <pod.den1804@gmail.com>
2017-08-28 00:06:29 +0300

    Start project

---
+++ WebCalc/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCalc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetEvents()
+        {
+            using (MyCalcDBEntities db = new MyCalcDBEntities())
+            {
+                var events = db.Events.ToList();
+                return new JsonResult
+                {
+                    Data = events,
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+        }
+    }
+}
